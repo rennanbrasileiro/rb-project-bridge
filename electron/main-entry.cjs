9d878e9aed65e04af43c1c3e3d7afb26e6969b7c
rennanbrasileiro <rennanbrasileiro@gmail.com>
2026-07-27 03:30:36 -0300

    Load functional workspace generator before application startup

--- electron/main-entry.cjs
+++ electron/main-entry.cjs
@@ -2,4 +2,5 @@
 
 require('./patches/review-history-patch.cjs');
 require('./patches/verification-integrity-patch.cjs');
+require('./patches/functional-workspace-patch.cjs');
 require('./main.cjs');
